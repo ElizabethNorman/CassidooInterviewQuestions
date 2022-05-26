2e2417ae8aedbe1e1a1046f3f4405d54e0dd0014
Elizabeth Norman <60582265+ElizabethNorman@users.noreply.github.com>
2022-05-25 22:41:04 -0700

    Fixing some parts of the may 16th question

--- 2022-05-16-MaximizedArray/src/Main.java
+++ 2022-05-16-MaximizedArray/src/Main.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /** @Author Elizabeth Norman, @spicedboi
- * I think this answer is so stupid, lmao. I'd like to come back to it if I get some inspiration, but at least this is
- * O(N) and works! We add the two arrays together and then return the top, unique elements.
+ * I think this answer is so stupid, lmao. I'd like to come back to it if I get some inspiration, but at least this
+ * isn't computationally expensive and does work! We add the two arrays together and then return the top, unique elements.
  *
  * Cassidoo Interview Question, May 16th
  * This week’s question:
@@ -32,30 +32,32 @@ public class Main
         System.out.println(ra);
     }
 
+    /* Base on the phrasing of the question I am not worrying about the case where two given arrays are of different
+     * length
+     *
+     */
     public static ArrayList<Integer> maximizedArray(int[] a1, int[] a2)
     {
-        ArrayList<Integer> ra = new ArrayList<Integer>();
+        ArrayList<Integer> joinedArray = new ArrayList<Integer>();
 
         for (int i = 0; i < a2.length; i ++)
         {
-            ra.add(a2[i]);
+            joinedArray.add(a2[i]);
+            joinedArray.add(a1[i]);
         }
-        for (int i = 0; i < a1.length; i++)
-        {
-            ra.add(a1[i]);
-        }
-        Collections.sort(ra);
 
-        ArrayList<Integer> raa = new ArrayList<Integer>();
+        Collections.sort(joinedArray);
 
-        for (int i = ra.size()-1; i >= a1.length - 1; i --)
+        ArrayList<Integer> returnArray = new ArrayList<Integer>();
+
+        for (int i = joinedArray.size()-1; i >= a1.length - 1; i --)
         {
-            if (raa.contains(ra.get(i)))
+            if (returnArray.contains(joinedArray.get(i)))
                 continue;
             else
-                raa.add(ra.get(i));
+                returnArray.add(joinedArray.get(i));
         }
-        return raa;
+        return returnArray;
     }
 
 }
